5c34eb1299b8cfc4b76fd6bd44ae0093cd562b49
Susan Tan <ssusantan9@gmail.com>
2019-04-04 02:46:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,4 +9,17 @@ class Shoe
     @brand = brand
   end 
   
+  def cobble
+    puts "Your shoe is as good as new!"
+  end 
+  
+  
+
+  #   it 'makes the shoe\'s condition new' do
+  #     shoe.condition = "old"
+  #     shoe.cobble
+  #     expect(shoe.condition).to eq("new")
+  #   end
+  # end
+  
 end 
