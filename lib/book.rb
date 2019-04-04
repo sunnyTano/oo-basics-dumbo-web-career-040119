d9f900c642cac7a786f4063c4756a0ca047d7029
Susan Tan <ssusantan9@gmail.com>
2019-04-04 02:41:13 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,7 +7,16 @@ class Book
     @title = title
   end 
   
+  def tu
 
   
+  describe '#turn_page' do
+    it 'can turn the page' do
+      expect($stdout).to receive(:puts).with("Flipping the page...wow, you read fast!")
+      book = Book.new("The World According to Garp")
+      book.turn_page 
+    end
+  end
+end
 end 
 
