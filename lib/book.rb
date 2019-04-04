e73a12ad4038383c05fe7d98f785fe22ce84d375
Susan Tan <ssusantan9@gmail.com>
2019-04-04 02:37:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,13 @@
 class Book 
   
+  attr_accessor :author, :page_count
+  attr_reader :title
+  
   def initialize(title)
     @title = title
   end 
   
-  def author=(author)
-    @author = author
-  end 
-  
-  def author
-    @author
-  end
+
   
 end 
 
